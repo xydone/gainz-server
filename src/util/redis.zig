b8a0910f27a0c712f81d14c975eb561560c0ba55
xydone <xydone@outlook.com>
2025-01-11 22:45:27 +0200

    feat: barebones redis client implementation

---
+++ src/util/redis.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+const log = std.log.scoped(.redis);
+
+pub const RedisClient = struct {
+    allocator: std.mem.Allocator,
+    connection: std.net.Stream,
+
+    pub fn init(allocator: std.mem.Allocator, host: []const u8, port: u16) !RedisClient {
+        const conn = try std.net.tcpConnectToHost(allocator, host, port);
+        return RedisClient{
+            .allocator = allocator,
+            .connection = conn,
+        };
+    }
+
+    pub fn deinit(self: *RedisClient) void {
+        self.connection.close();
+    }
+
+    pub fn sendCommand(self: *RedisClient, command: []const u8) ![]u8 {
+        try self.connection.writer().writeAll(command);
+        var buf: [1024]u8 = undefined;
+        const readBytes = try self.connection.reader().read(&buf);
+
+        return self.allocator.dupe(u8, buf[0..readBytes]);
+    }
+
+    pub fn set(self: *RedisClient, key: []const u8, value: []const u8) ![]u8 {
+        var buf: [1024]u8 = undefined;
+        const command = try std.fmt.bufPrint(&buf, "*3\r\n$3\r\nSET\r\n${d}\r\n{s}\r\n${d}\r\n{s}\r\n$2\r\n", .{ key.len, key, value.len, value });
+        return try sendCommand(self.connection, command);
+    }
+
+    pub fn setWithExpiry(self: *RedisClient, key: []const u8, value: []const u8, expiry: u32) ![]u8 {
+        if (expiry == 0) return error.BadExpiry;
+        var buf: [1024]u8 = undefined;
+        const command = try std.fmt.bufPrint(&buf, "*5\r\n$3\r\nSET\r\n${d}\r\n{s}\r\n${d}\r\n{s}\r\n$2\r\nEX\r\n${d}\r\n{}\r\n", .{ key.len, key, value.len, value, std.math.log10(expiry) + 1, expiry });
+        return try self.sendCommand(command);
+    }
+
+    pub fn get(self: *RedisClient, key: []const u8) ![]const u8 {
+        var buf: [1024]u8 = undefined;
+        const command = try std.fmt.bufPrint(&buf, "*2\r\n$3\r\nGET\r\n${d}\r\n{s}\r\n", .{ key.len, key });
+        const response = try self.sendCommand(command);
+        var it = std.mem.tokenizeSequence(u8, response, "\r\n");
+        const length = it.next().?;
+        if (std.mem.eql(u8, length, "$-1")) return error.KeyValuePairNotFound;
+        return it.next().?;
+    }
+};
