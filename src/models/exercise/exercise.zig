04a0564998123396653df3cfa961f4ea467bb437
xydone <xydone@outlook.com>
2025-07-17 02:18:21 +0300

    feat: exercise logging properly implemented with correct response and testing

--- src/models/exercise/exercise.zig
+++ src/models/exercise/exercise.zig
@@ -8,21 +8,6 @@ const Handler = @import("../../handler.zig");
 const rq = @import("../../request.zig");
 const log = std.log.scoped(.exercise_model);
 
-pub const ExerciseList = struct {
-    list: []Exercise,
-    allocator: std.mem.Allocator,
-
-    pub fn deinit(self: *ExerciseList) void {
-        self.allocator.free(self.list);
-    }
-};
-
-pub const Exercise = struct {
-    id: i32,
-    name: []const u8,
-    description: ?[]const u8,
-};
-
 pub const Create = struct {
     pub const Request = struct {
         name: []const u8,
@@ -31,73 +16,100 @@ pub const Create = struct {
         base_unit: []const u8,
         category_id: u32,
     };
-    pub const Response = Exercise;
+    pub const Response = struct {
+        id: i32,
+        created_by: i32,
+        name: []const u8,
+        description: ?[]const u8,
+        base_unit_id: i32,
+    };
     pub const Errors = error{ CannotCreate, CannotParseResult } || DatabaseErrors;
 
-    pub fn call(user_id: i32, database: *Pool, request: Request) anyerror!Response {
-        var conn = try database.acquire();
+    pub fn call(user_id: i32, database: *Pool, request: Request) Errors!Response {
+        var conn = database.acquire() catch return error.CannotAcquireConnection;
         defer conn.release();
 
-        var row = try conn.row(query_string, //
-            .{ user_id, request.name, request.description, request.base_amount, request.base_unit, request.category_id }) orelse return error.CannotCreate;
+        var row = conn.row(query_string, //
+            .{ user_id, request.name, request.description, request.base_amount, request.base_unit, request.category_id }) catch |err| {
+            const error_handler = ErrorHandler{ .conn = conn };
+            const error_data = error_handler.handle(err);
+            if (error_data) |data| ErrorHandler.printErr(data);
+            return error.CannotCreate;
+        } orelse return error.CannotCreate;
         defer row.deinit() catch {};
 
-        const id = row.get(i32, 0);
-        const name = row.get([]u8, 2);
-        const description = row.get(?[]u8, 3);
-        return Exercise{ .id = id, .name = name, .description = description };
+        return row.to(Response, .{}) catch return error.CannotParseResult;
     }
     const query_string =
         \\WITH
         \\inserted_exercise AS (
         \\INSERT INTO training.exercise (created_by, name, description)
         \\VALUES ($1, $2, $3)
-        \\RETURNING id, created_by, name,description
+        \\RETURNING id, created_by, name, description
         \\),
         \\inserted_unit AS (
         \\INSERT INTO training.exercise_unit (created_by, amount, unit, multiplier)
         \\SELECT $1, $4, $5, 1
         \\FROM inserted_exercise
+        \\RETURNING id AS unit_id
         \\),
         \\inserted_category AS (
         \\INSERT INTO training.exercise_has_category (exercise_id, category_id)
         \\SELECT id, $6
         \\FROM inserted_exercise
         \\)
-        \\SELECT * FROM inserted_exercise;
+        \\SELECT e.*, u.unit_id
+        \\FROM inserted_exercise e, inserted_unit u;
     ;
 };
 
 pub const GetAll = struct {
     pub const Request = struct {};
-    pub const Response = ExerciseList;
-    pub const Errors = error{ CannotGet, CannotParseResult } || DatabaseErrors;
+    pub const Response = struct {
+        list: []Exercise,
+        allocator: std.mem.Allocator,
 
-    pub fn call(allocator: std.mem.Allocator, user_id: i32, database: *Pool) anyerror!Response {
-        var conn = try database.acquire();
+        pub fn deinit(self: *Response) void {
+            self.allocator.free(self.list);
+        }
+    };
+
+    const Exercise = struct {
+        id: i32,
+        name: []const u8,
+        description: ?[]const u8,
+    };
+    pub const Errors = error{
+        CannotGet,
+        CannotParseResult,
+        OutOfMemory,
+    } || DatabaseErrors;
+
+    pub fn call(allocator: std.mem.Allocator, user_id: i32, database: *Pool) Errors!Response {
+        var conn = database.acquire() catch return error.CannotAcquireConnection;
         defer conn.release();
 
         var result = conn.queryOpts(query_string, .{user_id}, .{ .column_names = true }) catch |err| {
-            if (conn.err) |pg_err| {
-                log.err("severity: {s} |code: {s} | failure: {s}", .{ pg_err.severity, pg_err.code, pg_err.message });
-            }
-            return err;
+            const error_handler = ErrorHandler{ .conn = conn };
+            const error_data = error_handler.handle(err);
+            if (error_data) |data| ErrorHandler.printErr(data);
+            return error.CannotGet;
         };
         defer result.deinit();
         var response = std.ArrayList(Exercise).init(allocator);
-        while (try result.next()) |row| {
+        while (result.next() catch return error.CannotGet) |row| {
             const id = row.get(i32, 0);
 
             const name = row.getCol([]u8, "name");
             const description = row.getCol(?[]u8, "description");
 
-            try response.append(Exercise{
+            response.append(Exercise{
                 .id = id,
-                .name = try allocator.dupe(u8, name),
-                .description = if (description == null) null else try allocator.dupe(u8, description.?),
-            });
+                .name = allocator.dupe(u8, name) catch return error.OutOfMemory,
+                .description = if (description == null) null else allocator.dupe(u8, description.?) catch return error.OutOfMemory,
+            }) catch return error.OutOfMemory;
         }
-        return ExerciseList{ .list = try response.toOwnedSlice(), .allocator = allocator };
+        return Response{ .list = response.toOwnedSlice() catch return error.OutOfMemory, .allocator = allocator };
     }
     const query_string = "SELECT id,name, description FROM training.exercise WHERE created_by = $1";
 };
@@ -107,19 +119,40 @@ pub const LogExercise = struct {
         exercise_id: u32,
         unit_id: u32,
         value: f32,
-        notes: ?[]u8 = null,
+        notes: ?[]const u8 = null,
     };
-    pub fn call(ctx: *Handler.RequestContext, request: Request) anyerror!void {
-        var conn = try ctx.app.db.acquire();
+    pub const Response = struct {
+        id: i32,
+        created_at: i64,
+        created_by: i32,
+        exercise_id: i32,
+        value: f64,
+        unit_id: i32,
+        notes: ?[]const u8,
+    };
+    pub const Errors = error{
+        CannotLog,
+        CannotParseResult,
+    } || DatabaseErrors;
+    pub fn call(user_id: i32, database: *Pool, request: Request) Errors!Response {
+        var conn = database.acquire() catch return error.CannotAcquireConnection;
         defer conn.release();
 
-        _ = try conn.exec(query_string, .{ ctx.user_id, request.exercise_id, request.value, request.unit_id, request.notes });
+        const row = conn.row(query_string, .{ user_id, request.exercise_id, request.value, request.unit_id, request.notes }) catch |err| {
+            const error_handler = ErrorHandler{ .conn = conn };
+            const error_data = error_handler.handle(err);
+            if (error_data) |data| ErrorHandler.printErr(data);
+            return error.CannotLog;
+        } orelse return error.CannotLog;
+
+        return row.to(Response, .{}) catch return error.CannotParseResult;
     }
     const query_string =
         \\INSERT INTO
-        \\exercise_entry (created_by, exercise_id, value, unit_id, notes)
+        \\training.exercise_entry (created_by, exercise_id, value, unit_id, notes)
         \\VALUES
         \\($1, $2, $3, $4, $5)
+        \\RETURNING *
     ;
 };
 
@@ -162,5 +195,61 @@ test "API Exercise | Create" {
             benchmark.fail(err);
             return err;
         };
+    }
+}
+
+test "API Exercise | Log" {
+    const test_name = "API Exercise | Log";
+    //SETUP
+    const Benchmark = @import("../../tests/benchmark.zig");
+    const CreateCategory = @import("category.zig").Create;
+    const test_env = Tests.test_env;
+    var setup = try TestSetup.init(test_env.database, test_name);
+    const allocator = std.testing.allocator;
+    defer setup.deinit(allocator);
+
+    const category = try CreateCategory.call(setup.user.id, test_env.database, .{
+        .name = "Chest",
+    });
+    const create_request = Create.Request{
+        .name = test_name,
+        .category_id = @intCast(category.id),
+        .base_amount = 1,
+        .base_unit = "kg",
+    };
+    const create_response = try Create.call(setup.user.id, test_env.database, create_request);
+
+    // TEST
+    {
+        var benchmark = Benchmark.start(test_name);
+        defer benchmark.end();
+        const request = LogExercise.Request{
+            .exercise_id = @intCast(create_response.id),
+            .unit_id = @intCast(create_response.base_unit_id),
+            .value = 15,
+        };
+        const response = LogExercise.call(setup.user.id, test_env.database, request) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+
+        std.testing.expectEqual(@as(i32, @intCast(request.exercise_id)), response.exercise_id) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        std.testing.expectEqual(@as(i32, @intCast(request.unit_id)), response.unit_id) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        std.testing.expectEqual(request.value, response.value) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        if (request.notes) |notes| {
+            std.testing.expectEqualStrings(notes, response.notes.?) catch |err| {
+                benchmark.fail(err);
+                return err;
+            };
+        }
     }
 }
--- src/routes/exercise/entry.zig
+++ src/routes/exercise/entry.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 const httpz = @import("httpz");
 
 const Handler = @import("../../handler.zig");
-const rq = @import("../../request.zig");
 const rs = @import("../../response.zig");
 const LogExercise = @import("../../models/exercise/exercise.zig").LogExercise;
 
@@ -21,9 +20,96 @@ pub fn createEntry(ctx: *Handler.RequestContext, req: *httpz.Request, res: *http
         try rs.handleResponse(res, rs.ResponseError.body_missing_fields, null);
         return;
     };
-    LogExercise.call(ctx, exercise_entry) catch {
+    const response = LogExercise.call(ctx.user_id.?, ctx.app.db, exercise_entry) catch {
         try rs.handleResponse(res, rs.ResponseError.internal_server_error, null);
         return;
     };
     res.status = 200;
+
+    try res.json(response, .{});
+}
+
+const Tests = @import("../../tests/tests.zig");
+const TestSetup = Tests.TestSetup;
+
+test "Endpoint Exercise | Log" {
+    // SETUP
+    const test_name = "Endpoint Exercise | Log";
+    const ht = @import("httpz").testing;
+    const Create = @import("../../models/exercise/exercise.zig").Create;
+    const CreateCategory = @import("../../models/exercise/category.zig").Create;
+    const Benchmark = @import("../../tests/benchmark.zig");
+    const test_env = Tests.test_env;
+    const allocator = std.testing.allocator;
+
+    var user = try TestSetup.createUser(test_env.database, test_name);
+    defer user.deinit(allocator);
+
+    const category_request = CreateCategory.Request{ .name = "Chest" };
+
+    const category = try CreateCategory.call(user.id, test_env.database, category_request);
+
+    const create_request = Create.Request{
+        .name = test_name ++ " exercise",
+        .base_amount = 1,
+        .base_unit = test_name ++ "'s plates",
+        .category_id = @intCast(category.id),
+    };
+
+    const exercise = try Create.call(user.id, test_env.database, create_request);
+
+    const body = LogExercise.Request{
+        .exercise_id = @intCast(exercise.id),
+        .unit_id = @intCast(exercise.base_unit_id),
+        .value = 123,
+    };
+    const body_string = try std.json.stringifyAlloc(allocator, body, .{});
+    defer allocator.free(body_string);
+
+    var context = try TestSetup.createContext(user.id, allocator, test_env.database);
+    defer TestSetup.deinitContext(allocator, context);
+    // TEST
+    {
+        var benchmark = Benchmark.start(test_name);
+        defer benchmark.end();
+        var web_test = ht.init(.{});
+        defer web_test.deinit();
+
+        web_test.body(body_string);
+
+        createEntry(&context, web_test.req, web_test.res) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        web_test.expectStatus(200) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        const response_body = web_test.getBody() catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        const response = std.json.parseFromSlice(LogExercise.Response, allocator, response_body, .{}) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        defer response.deinit();
+
+        std.testing.expectEqual(user.id, response.value.created_by) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        std.testing.expectEqual(exercise.base_unit_id, response.value.unit_id) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        std.testing.expectEqual(exercise.id, response.value.exercise_id) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+        std.testing.expectEqual(body.value, response.value.value) catch |err| {
+            benchmark.fail(err);
+            return err;
+        };
+    }
 }
