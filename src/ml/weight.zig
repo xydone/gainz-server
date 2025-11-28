f40365a5462bca332884692c7efa26921f7a90a1
xydone <xydone@outlook.com>
2025-11-28 17:57:17 +0200

    feat(ml): improve weight model

---
+++ src/ml/weight.zig
@@ -0,0 +1,345 @@
+pub const Data = struct {
+    calories: f32,
+    carbs: f32,
+    sugar: f32,
+    protein: f32,
+    sodium: f32,
+    weight: f32,
+};
+
+pub const Features = struct {
+    calories_rolling: f32,
+    carbs_rolling: f32,
+    sugar_rolling: f32,
+    protein_rolling: f32,
+    sodium_rolling: f32,
+    previous_weight: f32,
+    weight_delta: f32,
+    weight: f32,
+
+    fn fromData(allocator: std.mem.Allocator, data: []Data, rolling_window: usize) ![]Features {
+        var calories = try allocator.alloc(f32, data.len);
+        defer allocator.free(calories);
+        var carbs = try allocator.alloc(f32, data.len);
+        defer allocator.free(carbs);
+        var sugar = try allocator.alloc(f32, data.len);
+        defer allocator.free(sugar);
+        var protein = try allocator.alloc(f32, data.len);
+        defer allocator.free(protein);
+        var sodium = try allocator.alloc(f32, data.len);
+        defer allocator.free(sodium);
+
+        for (data, 0..) |record, i| {
+            calories[i] = record.calories;
+            carbs[i] = record.carbs;
+            sugar[i] = record.sugar;
+            protein[i] = record.protein;
+            sodium[i] = record.sodium;
+        }
+
+        const calories_rolling = try rollingMean(allocator, calories, rolling_window);
+        defer allocator.free(calories_rolling);
+        const carbs_rolling = try rollingMean(allocator, carbs, rolling_window);
+        defer allocator.free(carbs_rolling);
+        const sugar_rolling = try rollingMean(allocator, sugar, rolling_window);
+        defer allocator.free(sugar_rolling);
+        const protein_rolling = try rollingMean(allocator, protein, rolling_window);
+        defer allocator.free(protein_rolling);
+        const sodium_rolling = try rollingMean(allocator, sodium, rolling_window);
+        defer allocator.free(sodium_rolling);
+
+        var processed_data = std.ArrayList(Features).empty;
+        defer processed_data.deinit(allocator);
+
+        // start at 1 as we don't know the previous weight of the first element
+        for (data[1..], 1..) |record, i| {
+            try processed_data.append(allocator, .{
+                .calories_rolling = calories_rolling[i - 1],
+                .carbs_rolling = carbs_rolling[i - 1],
+                .sugar_rolling = sugar_rolling[i - 1],
+                .protein_rolling = protein_rolling[i - 1],
+                .sodium_rolling = sodium_rolling[i - 1],
+                .previous_weight = data[i - 1].weight,
+                .weight_delta = record.weight - data[i - 1].weight,
+                .weight = record.weight,
+            });
+        }
+
+        return processed_data.toOwnedSlice(allocator);
+    }
+};
+pub const Train = struct {
+    pub const Response = struct {
+        r2: f32,
+        mae: f32,
+        created_at: i64,
+    };
+
+    pub const Errors = error{ CouldntSaveModel, XGBoostError, OutOfMemory };
+
+    pub fn run(allocator: std.mem.Allocator, absolute_path: []const u8, user_id: i32, data: []Data) Errors!Response {
+        const processed_data = try Features.fromData(allocator, data, 3);
+        defer allocator.free(processed_data);
+
+        const split = try trainTestSplit(processed_data, 0.3);
+        const train_data = split.train;
+        const test_data = split.@"test";
+
+        const dtrain, const train_labels = try createMatrix(allocator, train_data);
+        defer {
+            dtrain.deinit();
+            allocator.free(train_labels);
+        }
+        const dtest, const test_labels = try createMatrix(allocator, test_data);
+        defer {
+            dtest.deinit();
+            allocator.free(test_labels);
+        }
+
+        var dmats = [_]XGBoost.DMatrix.DMatrixHandle{ dtrain.handle, dtest.handle };
+        const booster = try XGBoost.Booster.init(&dmats, dmats.len);
+        defer booster.deinit();
+
+        try booster.setParam("objective", "reg:squarederror");
+
+        for (0..100) |i| {
+            try booster.updateOneIter(@intCast(i), dtrain);
+        }
+
+        // predict
+        const predicted_deltas = try booster.predict(dtest, 0, 0, 0);
+
+        var predicted_weights = try allocator.alloc(f32, test_data.len);
+        defer allocator.free(predicted_weights);
+
+        var actual_weights = try allocator.alloc(f32, test_data.len);
+        defer allocator.free(actual_weights);
+
+        for (test_data, 0..) |record, i| {
+            predicted_weights[i] = record.previous_weight + predicted_deltas[i];
+            actual_weights[i] = record.weight;
+        }
+
+        const r2 = r2Score(actual_weights, predicted_weights);
+        const mae = meanAbsoluteError(actual_weights, predicted_weights);
+        const created_at = std.time.microTimestamp();
+
+        const file_path = try std.fmt.allocPrintSentinel(
+            allocator,
+            "./models/model_{}_{}.ubj",
+            .{ user_id, created_at },
+            0,
+        );
+        defer allocator.free(file_path);
+
+        const data_dir = std.fs.openDirAbsolute(absolute_path, .{}) catch blk: {
+            std.fs.cwd().makeDir(absolute_path) catch return error.CouldntSaveModel;
+            break :blk std.fs.openDirAbsolute(absolute_path, .{}) catch return error.CouldntSaveModel;
+        };
+
+        // XGBoost's saveModel requires the folder to already be created
+        // if ./models cannot be opened, assume it is not present and create it
+        _ = data_dir.openDir("./models", .{}) catch blk: {
+            break :blk data_dir.makeDir("./models") catch return error.CouldntSaveModel;
+        };
+        booster.saveModel(file_path) catch return error.CouldntSaveModel;
+
+        return .{
+            .r2 = r2,
+            .mae = mae,
+            .created_at = created_at,
+        };
+    }
+    fn trainTestSplit(
+        data: []Features,
+        test_size: f32,
+    ) !struct { train: []const Features, @"test": []const Features } {
+        const test_count = @as(u32, @intFromFloat(@round(test_size * @as(f32, @floatFromInt(data.len)))));
+        const train_count = data.len - test_count;
+
+        return .{
+            .train = data[0..train_count],
+            .@"test" = data[train_count..],
+        };
+    }
+};
+
+pub const Predict = struct {
+    pub const Response = struct {
+        predicted_delta: f32,
+        predicted_weight: f32,
+    };
+    pub fn run(allocator: std.mem.Allocator, user_id: i32, data_dir: []const u8, data: []Data) !Response {
+        const window_size = 3;
+
+        if (data.len < window_size) {
+            return error.NotEnoughData;
+        }
+        const file_name = getMostRecentModel(allocator, data_dir, user_id) catch return error.NoModelFound;
+        defer allocator.free(file_name);
+
+        const booster = XGBoost.Booster.initModelFromFile(file_name) catch return error.CouldntInitModel;
+        defer booster.deinit();
+
+        const window = data[data.len - window_size ..];
+
+        var sum_calories: f32 = 0;
+        var sum_carbs: f32 = 0;
+        var sum_sugar: f32 = 0;
+        var sum_protein: f32 = 0;
+        var sum_sodium: f32 = 0;
+
+        for (window) |d| {
+            sum_calories += d.calories;
+            sum_carbs += d.carbs;
+            sum_sugar += d.sugar;
+            sum_protein += d.protein;
+            sum_sodium += d.sodium;
+        }
+
+        const f_size = @as(f32, @floatFromInt(window_size));
+        const last_known_weight = data[data.len - 1].weight;
+
+        const next_feature = Features{
+            .calories_rolling = sum_calories / f_size,
+            .carbs_rolling = sum_carbs / f_size,
+            .sugar_rolling = sum_sugar / f_size,
+            .protein_rolling = sum_protein / f_size,
+            .sodium_rolling = sum_sodium / f_size,
+            .previous_weight = last_known_weight,
+            // 0 as they are the ones being predicted
+            .weight_delta = 0,
+            .weight = 0,
+        };
+
+        const feature_slice = [_]Features{next_feature};
+
+        const dmat, const labels = try createMatrix(allocator, &feature_slice);
+        defer {
+            allocator.free(labels);
+            dmat.deinit();
+        }
+
+        const predicted_deltas = try booster.predict(dmat, 0, 0, 0);
+
+        if (predicted_deltas.len > 0) {
+            const predicted_delta = predicted_deltas[0];
+            const predicted_weight = last_known_weight + predicted_delta;
+
+            return .{
+                .predicted_weight = predicted_weight,
+                .predicted_delta = predicted_delta,
+            };
+        } else {
+            return error.PredictionFailed;
+        }
+    }
+};
+
+// utility functions
+fn rollingMean(allocator: std.mem.Allocator, data: []const f32, window_size: usize) ![]f32 {
+    var results = try allocator.alloc(f32, data.len);
+    for (data, 0..) |_, i| {
+        const start = if (i < window_size) 0 else i - window_size + 1;
+        const window = data[start .. i + 1];
+        var sum: f32 = 0;
+        for (window) |val| {
+            sum += val;
+        }
+        results[i] = sum / @as(f32, @floatFromInt(window.len));
+    }
+    return results;
+}
+
+fn r2Score(actual: []const f32, predicted: []const f32) f32 {
+    var sum_actual: f32 = 0;
+    for (actual) |y| {
+        sum_actual += y;
+    }
+    const mean_actual = sum_actual / @as(f32, @floatFromInt(actual.len));
+
+    var ss_total: f32 = 0;
+    var ss_res: f32 = 0;
+    for (actual, 0..) |y_true, i| {
+        const y_pred = predicted[i];
+        ss_total += (y_true - mean_actual) * (y_true - mean_actual);
+        ss_res += (y_true - y_pred) * (y_true - y_pred);
+    }
+
+    return 1 - (ss_res / ss_total);
+}
+
+/// Caller owns memory
+fn getMostRecentModel(allocator: std.mem.Allocator, data_dir: []const u8, user_id: i32) ![:0]u8 {
+    const model_dir = try std.fmt.allocPrint(allocator, "{s}models", .{
+        data_dir,
+    });
+    defer allocator.free(model_dir);
+
+    const dir = try std.fs.openDirAbsolute(model_dir, .{ .iterate = true });
+    var it = dir.iterate();
+
+    var most_recent_model_date: ?i64 = null;
+    while (try it.next()) |file| {
+        var name_it = std.mem.tokenizeScalar(u8, file.name, '_');
+        // the prefix should be "model"
+        const prefix = name_it.next() orelse continue;
+        if (!std.mem.eql(u8, prefix, "model")) continue;
+        // the user id should match with the caller user id
+        const user_id_str = name_it.next() orelse continue;
+        const file_user_id = std.fmt.parseInt(i32, user_id_str, 10) catch continue;
+        if (user_id != file_user_id) continue;
+        const timestamp_str = blk: {
+            const t = name_it.next() orelse continue;
+            break :blk std.mem.trimEnd(u8, t, ".ubj");
+        };
+        const timestamp = std.fmt.parseInt(i64, timestamp_str, 10) catch continue;
+        if (most_recent_model_date) |date| {
+            if (timestamp > date) most_recent_model_date = timestamp;
+        } else {
+            most_recent_model_date = timestamp;
+        }
+    }
+    if (most_recent_model_date == null) return error.CouldntFindModel;
+
+    return try std.fmt.allocPrintSentinel(allocator, "{s}/model_{}_{}.ubj", .{
+        model_dir,
+        user_id,
+        most_recent_model_date.?,
+    }, 0);
+}
+
+fn meanAbsoluteError(actual: []const f32, predicted: []const f32) f32 {
+    var sum_abs_error: f32 = 0;
+    for (actual, 0..) |y_true, i| {
+        sum_abs_error += @abs(y_true - predicted[i]);
+    }
+    return sum_abs_error / @as(f32, @floatFromInt(actual.len));
+}
+
+/// Caller is responsible for the matrix and the slice
+fn createMatrix(allocator: std.mem.Allocator, data: []const Features) !struct { XGBoost.DMatrix, []f32 } {
+    // subtract 1 because of weight and weight delta are not features
+    const num_features = @typeInfo(Features).@"struct".fields.len - 2;
+
+    var feat = try allocator.alloc(f32, data.len * num_features);
+    defer allocator.free(feat);
+    var labels = try allocator.alloc(f32, data.len);
+
+    for (data, 0..) |record, i| {
+        feat[i * num_features + 0] = record.calories_rolling;
+        feat[i * num_features + 1] = record.carbs_rolling;
+        feat[i * num_features + 2] = record.sugar_rolling;
+        feat[i * num_features + 3] = record.protein_rolling;
+        feat[i * num_features + 4] = record.sodium_rolling;
+        feat[i * num_features + 5] = record.previous_weight;
+        labels[i] = record.weight_delta;
+    }
+
+    const dmat = try XGBoost.DMatrix.initFromMatrix(feat, data.len, num_features, -1, .{});
+    try dmat.setFloatInfo("label", labels);
+
+    return .{ dmat, labels };
+}
+const XGBoost = @import("xgboost");
+const std = @import("std");
--- src/routes/analytics/weight.zig
+++ src/routes/analytics/weight.zig
@@ -1,9 +1,11 @@
 pub const endpoint_data = [_]EndpointData{
     Train.endpoint_data,
+    Predict.endpoint_data,
 };
 
 pub inline fn init(router: *httpz.Router(*Handler, *const fn (*Handler.RequestContext, *httpz.request.Request, *httpz.response.Response) anyerror!void)) void {
     Train.init(router);
+    Predict.init(router);
 }
 
 const Train = Endpoint(struct {
@@ -27,59 +29,25 @@ const Train = Endpoint(struct {
         .route_data = .{ .restricted = true },
     };
     pub fn call(ctx: *Handler.RequestContext, request: EndpointRequest(Body, void, void), res: *httpz.Response) anyerror!void {
-        const breakdown_request: GetBreakdownModel.Request = .{
-            .range_start = request.body.range_start,
-            .range_end = request.body.range_end,
-        };
-
-        const get_breakdown_response = GetBreakdownModel.call(res.arena, ctx.user_id.?, ctx.app.db, breakdown_request) catch {
-            handleResponse(res, ResponseError.internal_server_error, null);
-            return;
-        };
-
-        const get_weight_request: MeasurementGetInRange.Request = .{
-            .measurement_type = .weight,
-            .range_start = request.body.range_start,
-            .range_end = request.body.range_end,
-        };
-
-        const measurements = MeasurementGetInRange.call(ctx.user_id.?, res.arena, ctx.app.db, get_weight_request) catch {
-            handleResponse(res, ResponseError.internal_server_error, null);
-            return;
-        };
-
-        var combined_values: std.ArrayList(TrainML.Data) = .empty;
-        defer combined_values.deinit(res.arena);
-
-        var measurements_by_day: std.StringHashMap(f32) = .init(res.arena);
-        defer measurements_by_day.deinit();
-
-        for (measurements) |measurement| {
-            const datetime = try zdt.Datetime.fromUnix(measurement.created_at, .microsecond, null);
-            var writer = std.Io.Writer.Allocating.init(res.arena);
-            try datetime.toString("%d-%m-%YYYY", &writer.writer);
-            try measurements_by_day.put(try writer.toOwnedSlice(), @floatCast(measurement.value));
-        }
-
-        // find matching dates
-        for (get_breakdown_response) |breakdown| {
-            const datetime = try zdt.Datetime.fromUnix(breakdown.created_at, .microsecond, null);
-            var writer = std.Io.Writer.Allocating.init(res.arena);
-            try datetime.toString("%d-%m-%YYYY", &writer.writer);
-            // if there is a date, append the combined values
-            if (measurements_by_day.get(try writer.toOwnedSlice())) |weight| {
-                try combined_values.append(res.arena, .{
-                    .weight = weight,
-                    .calories = @floatCast(breakdown.nutrients.calories),
-                    .protein = @floatCast(breakdown.nutrients.protein.?),
-                    .sugar = @floatCast(breakdown.nutrients.sugar.?),
-                    .carbs = @floatCast(breakdown.nutrients.carbs.?),
-                });
+        const data = collectData(ctx, res.arena, request.body.range_start, request.body.range_end) catch |err| {
+            switch (err) {
+                error.NoFoodEntries => {
+                    handleResponse(res, ResponseError.not_found, "Food entries data missing");
+                    return;
+                },
+                error.NoMeasurements => {
+                    handleResponse(res, ResponseError.not_found, "Weight data missing.");
+                    return;
+                },
+                else => {
+                    handleResponse(res, ResponseError.internal_server_error, null);
+                    return;
+                },
             }
-        }
+        };
+        defer res.arena.free(data);
 
-        const response = TrainML.run(res.arena, ctx.app.env.DATA_DIR, ctx.user_id.?, combined_values.items) catch |err| {
-            std.debug.print("error: {}\n", .{err});
+        const response = TrainML.run(res.arena, ctx.app.env.DATA_DIR, ctx.user_id.?, data) catch |err| {
             return err;
         };
 
@@ -88,6 +56,118 @@ const Train = Endpoint(struct {
         try res.json(result, .{});
     }
 });
+
+const Predict = Endpoint(struct {
+    const Body = struct {
+        /// The most recent date
+        date: []const u8,
+    };
+    const Response = PredictML.Response;
+    pub const endpoint_data: EndpointData = .{
+        .Request = .{ .Body = Body },
+        .Response = Response,
+        .method = .POST,
+        .path = "/api/user/analytics/weight/predict",
+        .route_data = .{ .restricted = true },
+    };
+    pub fn call(ctx: *Handler.RequestContext, request: EndpointRequest(Body, void, void), res: *httpz.Response) anyerror!void {
+        const end_date = zdt.Datetime.fromString(request.body.date, "%Y-%m-%d") catch {
+            //TODO: handle date parsing and verification inside handler
+            handleResponse(res, ResponseError.bad_request, "Date is not valid");
+            return;
+        };
+        const start_datetime = zdt.Datetime.sub(&end_date, zdt.Duration.fromTimespanMultiple(14, .day)) catch {
+            handleResponse(res, ResponseError.bad_request, "Date is not valid");
+            return;
+        };
+        var writer = std.Io.Writer.Allocating.init(res.arena);
+        defer writer.deinit();
+
+        start_datetime.toString("%Y-%m-%d", &writer.writer) catch {
+            handleResponse(res, ResponseError.bad_request, "Date is not valid");
+            return;
+        };
+        const start_date = try writer.toOwnedSlice();
+        defer res.arena.free(start_date);
+
+        const data = collectData(ctx, res.arena, start_date, request.body.date) catch |err| {
+            switch (err) {
+                error.NoFoodEntries => {
+                    handleResponse(res, ResponseError.not_found, "Food entries data missing");
+                    return;
+                },
+                error.NoMeasurements => {
+                    handleResponse(res, ResponseError.not_found, "Weight data missing.");
+                    return;
+                },
+                else => {
+                    handleResponse(res, ResponseError.internal_server_error, null);
+                    return;
+                },
+            }
+        };
+
+        const prediction = try PredictML.run(res.arena, ctx.user_id.?, ctx.app.env.DATA_DIR, data);
+        try res.json(prediction, .{});
+    }
+});
+
+/// Caller must free
+fn collectData(
+    ctx: *Handler.RequestContext,
+    allocator: std.mem.Allocator,
+    range_start: []const u8,
+    range_end: []const u8,
+) ![]Data {
+    const breakdown_request: GetBreakdownModel.Request = .{
+        .range_start = range_start,
+        .range_end = range_end,
+    };
+    const get_breakdown_response = GetBreakdownModel.call(allocator, ctx.user_id.?, ctx.app.db, breakdown_request) catch return error.NoFoodEntries;
+    defer allocator.free(get_breakdown_response);
+
+    const get_weight_request: MeasurementGetInRange.Request = .{
+        .measurement_type = .weight,
+        .range_start = range_start,
+        .range_end = range_end,
+    };
+
+    const measurements = MeasurementGetInRange.call(ctx.user_id.?, allocator, ctx.app.db, get_weight_request) catch return error.NoMeasurements;
+    defer allocator.free(measurements);
+
+    var combined_values: std.ArrayList(Data) = .empty;
+    defer combined_values.deinit(allocator);
+
+    var measurements_by_day: std.StringHashMap(f32) = .init(allocator);
+    defer measurements_by_day.deinit();
+
+    for (measurements) |measurement| {
+        const datetime = try zdt.Datetime.fromUnix(measurement.created_at, .microsecond, null);
+        var writer = std.Io.Writer.Allocating.init(allocator);
+        try datetime.toString("%d-%m-%YYYY", &writer.writer);
+        try measurements_by_day.put(try writer.toOwnedSlice(), @floatCast(measurement.value));
+    }
+
+    // find matching dates
+    for (get_breakdown_response) |breakdown| {
+        const datetime = try zdt.Datetime.fromUnix(breakdown.created_at, .microsecond, null);
+        var writer = std.Io.Writer.Allocating.init(allocator);
+        try datetime.toString("%d-%m-%YYYY", &writer.writer);
+        // if there is a date, append the combined values
+        if (measurements_by_day.get(try writer.toOwnedSlice())) |weight| {
+            try combined_values.append(allocator, .{
+                .weight = weight,
+                .calories = @floatCast(breakdown.nutrients.calories),
+                .protein = @floatCast(breakdown.nutrients.protein.?),
+                .sugar = @floatCast(breakdown.nutrients.sugar.?),
+                .carbs = @floatCast(breakdown.nutrients.carbs.?),
+                .sodium = @floatCast(breakdown.nutrients.sodium.?),
+            });
+        }
+    }
+
+    return combined_values.toOwnedSlice(allocator);
+}
 
 const std = @import("std");
 
@@ -103,7 +183,10 @@ const handleResponse = @import("../../response.zig").handleResponse;
 
 const types = @import("../../types.zig");
 
-const TrainML = @import("../../ml/train.zig");
+const Data = @import("../../ml/weight.zig").Data;
+const Features = @import("../../ml/weight.zig").Features;
+const TrainML = @import("../../ml/weight.zig").Train;
+const PredictML = @import("../../ml/weight.zig").Predict;
 
 const Endpoint = @import("../../endpoint.zig").Endpoint;
 const EndpointRequest = @import("../../endpoint.zig").EndpointRequest;
